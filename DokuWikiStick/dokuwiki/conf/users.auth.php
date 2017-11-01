b0c3eae1e9c377f72c0a3ff980959da60a3a2bce
langchenglc <langchenglc@qq.com>
2017-11-01 15:17:06 +0800

    add new files

--- DokuWikiStick/dokuwiki/conf/users.auth.php
+++ DokuWikiStick/dokuwiki/conf/users.auth.php
@@ -9,9 +9,9 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-root:$1$Dq73gqYH$rvHM7xSOUl60J5OMJeKuc1:root:gonglc@skyon.com.cn:admin,user
 wangzj:$1$xlph7vcK$OqrYMJ3APukuxLsOz7s9Y1:wangzj:wangzj@skyon.com.cn:user
 dongj:$1$YTImkdTf$xnkjATuyPsM/O..tmZm9Q/:dongj:dongj@skyon.com.cn:user
 liuqq:$1$YxWww6iI$LGn1S7nQ6U9.wt8Qckay0/:liuqq:liuqq@skyon.com.cn:user
 wenzy:$1$7h9RP36Q$b4sUpKnO0WSXl5CFPKwmO1:wenzy:wenzy@skyon.com.cn:user
 gonglc:$1$PZoT5O28$1BsFBIGzvHDmz.yjXm4vY1:gonglc:gonglc@skyon.com.cn:user,admin
+root:$1$fzLFdbdw$vF1BpYnP7IGiJTNUXrkiC1:root:gonglc@skyon.com.cn:admin,user
